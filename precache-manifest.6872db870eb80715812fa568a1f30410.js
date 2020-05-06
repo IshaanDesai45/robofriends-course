a4035e7c9607b9f145bb321d5f62d1473369de14
IshaanDesai45 <46575231+IshaanDesai45@users.noreply.github.com>
2020-05-06 13:04:09 +0530

    Updates

---
+++ precache-manifest.6872db870eb80715812fa568a1f30410.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc3a309315e75e067e7f0a72d073740",
+    "url": "/robofriends-course/index.html"
+  },
+  {
+    "revision": "8bb6d19fff4e65f6f7a6",
+    "url": "/robofriends-course/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b35d8b7301b708bfe785",
+    "url": "/robofriends-course/static/css/main.2cac127d.chunk.css"
+  },
+  {
+    "revision": "8bb6d19fff4e65f6f7a6",
+    "url": "/robofriends-course/static/js/2.43071c07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends-course/static/js/2.43071c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b35d8b7301b708bfe785",
+    "url": "/robofriends-course/static/js/main.3351c603.chunk.js"
+  },
+  {
+    "revision": "8c6e1af235eb53fcaf11",
+    "url": "/robofriends-course/static/js/runtime-main.45d31b52.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-course/static/media/SEGA.12a3db8b.woff"
+  }
+]);
